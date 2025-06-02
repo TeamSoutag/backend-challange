ca6878bb9d6cc213d565a941006fb66f9b600a2a
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2025-06-02 13:28:25 -0300

    Create a simple configuration to handle messages and errors in the application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def bootstrap_alert_class(flash_type)
+    case flash_type.to_sym
+    when :notice then 'success'
+    when :alert  then 'danger'
+    when :error  then 'danger'
+    else flash_type.to_s
+    end
+  end
 end
