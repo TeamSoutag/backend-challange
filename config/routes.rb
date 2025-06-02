f740f2a87017a5b43fc6b8864bdd4d436d91c14d
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2025-06-02 13:13:49 -0300

    Create the routes and define the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'refuelings#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users do
+    resource :wallet, only: %i[create show update]
+  end
+
+  resources :refuelings
+  resources :gas_stations
+  resources :products
 end
